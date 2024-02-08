6a144acf30fd0a2890aa2eeef167b19b7815c325
cs572go <tushartushar572@gmail.com>
2024-02-08 13:09:10 +0530

    bully

--- LCA2/bully.cpp
+++ LCA2/bully.cpp
@@ -1,80 +1,100 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-class processNode
-{
+class processNode {
 public:
     bool isAlive;
     bool hasStartedElection;
     int id;
     int coordinator;
 
-    processNode(int idx)
-    {
-        this->isAlive = 1;
-        this->hasStartedElection = 0;
+    processNode(int idx) {
+        this->isAlive = true; // Use true/false for boolean values.
+        this->hasStartedElection = false;
         this->id = idx;
-        this->coordinator = 10;
-        // this->next = nullptr;
-    }
-    void disableNode()
-    {
-        this->isAlive = 0;
-        this->hasStartedElection = 0;
-        cout<<"Disabled node: "<<this->id;
+        this->coordinator = -1; // Initialize with an invalid value or 0, if no coordinator initially.
     }
 
-    void displayNode()
-    {
-        cout << "Node ID: " << this->id << ", alive: " << this->isAlive << " elec_st?: " << this->hasStartedElection << " boss: " << this->coordinator << endl;
+    void disableNode() {
+        this->isAlive = false;
+        this->hasStartedElection = false;
+        cout << "Disabled node: " << this->id << endl;
+    }
+
+    void displayNode() const {
+        cout << "Node ID: " << this->id << ", alive: " << this->isAlive << ", elec_st?: " << this->hasStartedElection << ", boss: " << this->coordinator << endl;
     }
 };
 
-int main()
-{
+void startElection(list<processNode>& nodes, int nodeId) {
+    int maxId = -1;
+    for (auto& node : nodes) {
+        if (node.id > maxId && node.isAlive) {
+            maxId = node.id;
+        }
+    }
+    // Set the coordinator to the node with the maxId
+    for (auto& node : nodes) {
+        if (node.isAlive) {
+            node.coordinator = maxId;
+        }
+    }
+    cout << "Election completed. New coordinator is Node ID: " << maxId << endl;
+}
+
+int main() {
     cout << "Election Algo Simulation" << endl;
     list<processNode> nodes;
     int choice = 100;
-    do
-    {
+    do {
         cout << "Menu" << endl;
         cout << "1. Add nodes" << endl;
         cout << "2. Disable a node" << endl;
         cout << "3. Display nodes" << endl;
-        cout << "4. Election" << endl;
+        cout << "4. Start Election" << endl;
         cout << "0. Exit" << endl;
         cin >> choice;
 
-        switch (choice)
-        {
-        case 1:
-            int nodestoadd;
-            cout<<"Enter number of nodes to add: ";
-            cin>>nodestoadd;
-            for (int i = 0; i < nodestoadd; i++)
-            {
-                int idx;
-                cout<<"Enter id: ";
-                cin>>idx;
-                processNode temp = processNode(idx);
-                nodes.push_back(temp);
-            }
-            break;
-        case 2:
-            int nodetodisable;
-            cout<<"Enter node to disable: ";
-            cin>>nodetodisable;
-            break;
-        case 3:
-            for(auto it=nodes.begin(); it!=nodes.end(); ){
-                it.displayNode;
-            }
-        default:
-            cout<<"Enter valid choice"<<endl;
-            break;
+        switch (choice) {
+            case 1: {
+                int nodestoadd;
+                cout << "Enter number of nodes to add: ";
+                cin >> nodestoadd;
+                for (int i = 0; i < nodestoadd; i++) {
+                    int idx;
+                    cout << "Enter id: ";
+                    cin >> idx;
+                    processNode temp = processNode(idx);
+                    nodes.push_back(temp);
+                }
+            } break;
+            case 2: {
+                int nodetodisable;
+                cout << "Enter node id to disable: ";
+                cin >> nodetodisable;
+                for (auto& node : nodes) {
+                    if (node.id == nodetodisable) {
+                        node.disableNode();
+                        break;
+                    }
+                }
+            } break;
+            case 3:
+                for (const auto& node : nodes) {
+                    node.displayNode();
+                }
+                break;
+            case 4:
+                int nodeId;
+                cout << "Enter node id to start election: ";
+                cin >> nodeId;
+                startElection(nodes, nodeId);
+                break;
+            default:
+                cout << "Enter valid choice." << endl;
+                break;
         }
-    }while (choice != 0);
+    } while (choice != 0);
 
     return 0;
 }
